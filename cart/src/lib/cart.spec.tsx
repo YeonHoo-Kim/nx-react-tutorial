c0c78f1e51041114936f45293c6ed1cdde36587e
Yeonhoo-Kim <kywho0915@naver.com>
2023-03-29 16:33:26 +0900

    [test] react-router-dom 컴포넌트를 테스팅 하기 위한 MemoryRouter 추가

--- cart/src/lib/cart.spec.tsx
+++ cart/src/lib/cart.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import Cart from './cart';
 
 describe('Cart', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Cart />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
--- src/app/shop/shop.spec.tsx
+++ src/app/shop/shop.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import Shop from './shop';
 
 describe('Shop', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Shop />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <Shop />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
